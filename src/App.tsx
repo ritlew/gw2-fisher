f8e7e339dc93ed6ff80f2113979ae457b79eedab
Samuel <samlewis@mailbox.org>
2022-09-05 13:23:48 -0400

    Add github button

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import {
   Box,
   Button,
   HStack,
+  IconButton,
 } from '@chakra-ui/react'
 
 // local
 import FishToJson from './FishToJson'
 import Tracker from './Tracker'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
+import { FaGithub } from 'react-icons/fa'
 
 const theme = extendTheme({
   initialColorMode: 'dark',
@@ -28,16 +30,30 @@ export const App = () => {
       <Container maxW="container.xl">
         <HStack pt={4} justifyContent="space-between">
           <Box />
-          <HStack>
-            <Button onClick={() => setPage('tracker')}>Tracker</Button>
-            <Button onClick={() => setPage('input')}>Input</Button>
-          </HStack>
+          {process.env.NODE_ENV === 'development' && (
+            <HStack>
+              <Button onClick={() => setPage('tracker')}>Tracker</Button>
+              <Button onClick={() => setPage('input')}>Input</Button>
+            </HStack>
+          )}
           <Box>
+            <IconButton
+              aria-label="Github Link"
+              title="View on GitHub"
+              variant="ghost"
+              onClick={() =>
+                window.open('https://github.com/ritlew/gw2-fisher')
+              }
+            >
+              <FaGithub />
+            </IconButton>
             <ColorModeSwitcher />
           </Box>
         </HStack>
         {page === 'tracker' && <Tracker />}
-        {page === 'input' && <FishToJson />}
+        {process.env.NODE_ENV === 'development' && page === 'input' && (
+          <FishToJson />
+        )}
       </Container>
     </ChakraProvider>
   )
