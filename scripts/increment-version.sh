b52d4d59603653044d53592ae76f5d518031ea56
Samuel <samlewis@mailbox.org>
2022-09-20 00:35:06 -0400

    Push tags on version increment

--- scripts/increment-version.sh
+++ scripts/increment-version.sh
@@ -1,27 +1,30 @@
-version=""
+#!/bin/bash
 
-while [ "$version" == "" ]
+version_type=""
+
+while [ "$version_type" == "" ]
 do
   echo "Is this a (1) Major, (2) Minor, (3, default) Patch, or (4) do not increment version? "
 
   read input
 
   if [ "$input" == "1" ]; then
-    version="major"
+    version_type="major"
   elif [ "$input" == "2" ]; then
-    version="minor"
+    version_type="minor"
   elif [ -z "$input" ] || [ "$input" == "3" ]; then 
-    version="patch"
+    version_type="patch"
   elif [ "$input" == "4" ]; then
-    version="none"
+    version_type="none"
   else
     echo -e "Invalid option\n"
   fi
 done
 
-if [ "$version" != "none" ]; then
-  npm version $version
+if [ "$version_type" != "none" ]; then
+  version=$(npm version $version_type)
   git push
+  git push origin v$version
 fi
 
 echo "REACT_APP_VERSION=$(node -p "require('./package.json').version")" > .env.local
